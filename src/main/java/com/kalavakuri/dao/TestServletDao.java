cbb8339bbd24d359a7efa210a3299805b73fd9cd
rkalavak <kalavakurirenu@gmail.com>
2019-05-13 07:32:39 +0530

    Output message changed.

--- src/main/java/com/kalavakuri/dao/TestServletDao.java
+++ src/main/java/com/kalavakuri/dao/TestServletDao.java
@@ -3,6 +3,6 @@ package com.kalavakuri.dao;
 public class TestServletDao {
 	
 	public void print() {
-		System.out.println("Output message...");
+		System.out.println("Output message changed...");
 	}
 }
